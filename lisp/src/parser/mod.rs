46e439af474ac1b3d00e9bc7f92b48c32d5098b5
Anton Johansson <antojohansson1@gmail.com>
2022-11-20 00:04:34 +0100

    Updated the lexer to handle string and remade tests

--- lisp/src/parser/mod.rs
+++ lisp/src/parser/mod.rs
@@ -1,18 +1,26 @@
 use super::lexer;
 use std::collections::VecDeque;
 
+//This is like a match_entry but not supposed to generate an error
+fn peek_match(input: &mut VecDeque<lexer::Entry>, t: lexer::Token) -> bool {
+    if peek(input, t.clone()) {
+        match_entry(input, t)
+    }
+    else {
+        false
+    }
+}
 
 fn peek(input: &mut VecDeque<lexer::Entry>, t: lexer::Token) -> bool {
     match input.get(0) {
         Some(v) => {
             if v.t == t {
                 true
-            }
-            else {
+            } else {
                 false
             }
-        },
-        _ => false
+        }
+        _ => false,
     }
 }
 
@@ -21,87 +29,105 @@ fn match_entry(input: &mut VecDeque<lexer::Entry>, t: lexer::Token) -> bool {
         match input.pop_front() {
             Some(e) => {
                 input.push_back(e);
-                    true
-            },
-            None => false //This should never happen due to the previous check
+                true
+            }
+            None => false, //This should never happen due to the previous check
         }
-    }
-    else {
+    } else {
         true
     }
-    
 }
 
 fn entry(input: &mut VecDeque<lexer::Entry>) -> bool {
     if peek(input, lexer::Token::Open) {
         expression_list(input)
-    }
-    else if peek(input, lexer::Token::Pure) {
+    } else if peek(input, lexer::Token::Pure) {
         list(input)
-    }
-    else {
-        match_entry(input, lexer::Token::Number) ||
-        match_entry(input, lexer::Token::Id) ||
-        match_entry(input, lexer::Token::Float)
+    } else {
+        match_entry(input, lexer::Token::Number)
+            || match_entry(input, lexer::Token::Id)
+            || match_entry(input, lexer::Token::Float)
     }
 }
 
 fn entry_list(input: &mut VecDeque<lexer::Entry>) -> bool {
     if peek(input, lexer::Token::Close) {
         true
-    }
-    else {
-        entry(input) && 
-        entry_list(input)
+    } else {
+        entry(input) && entry_list(input)
     }
 }
 
 fn list(input: &mut VecDeque<lexer::Entry>) -> bool {
-    match_entry(input, lexer::Token::Pure) &&
-    match_entry(input, lexer::Token::Open) &&
-    entry_list(input) &&
-    match_entry(input, lexer::Token::Close)
+    match_entry(input, lexer::Token::Pure)
+        && match_entry(input, lexer::Token::Open)
+        && entry_list(input)
+        && match_entry(input, lexer::Token::Close)
+}
+
+fn operator(input: &mut VecDeque<lexer::Entry>) -> bool {
+    peek_match(input, lexer::Token::Plus) ||
+    peek_match(input, lexer::Token::Minus) ||
+    peek_match(input, lexer::Token::Equal) ||
+    peek_match(input, lexer::Token::Less) ||
+    peek_match(input, lexer::Token::Mult) ||
+    peek_match(input, lexer::Token::Div) ||
+    peek_match(input, lexer::Token::Not) ||
+    peek_match(input, lexer::Token::And) ||
+    peek_match(input, lexer::Token::Or) 
+
 }
 
 fn func(input: &mut VecDeque<lexer::Entry>) -> bool {
-    panic!("TODO THIS");
+    match input.get(0) {
+        Some(e) => {
+            match e.lexeme.chars().nth(0) {
+                Some(t) => {
+                    if lexer::valid_id_char(t) {
+                        match_entry(input, lexer::Token::Id)
+                    }
+                    else {
+                        operator(input)
+                    }
+                },
+                _ => operator(input)
+            }
+        }, 
+        _ => { operator(input)
+    }
+    }
 }
 
 fn evals(input: &mut VecDeque<lexer::Entry>) -> bool {
-    match_entry(input, lexer::Token::Open) &&
-    func(input) &&
-    entry_list(input) &&
-    match_entry(input, lexer::Token::Close)
+    match_entry(input, lexer::Token::Open)
+        && func(input)
+        && entry_list(input)
+        && match_entry(input, lexer::Token::Close)
 }
 
-fn expression_list(input: &mut VecDeque<lexer::Entry>) -> bool{
-    expression(input) && 
-    {
+fn expression_list(input: &mut VecDeque<lexer::Entry>) -> bool {
+    expression(input) && {
         if peek(input, lexer::Token::Open) {
             expression_list(input)
+        } else {
+            true
         }
-        else { true }
     }
-
 }
 
-fn expression(input: &mut VecDeque<lexer::Entry>) ->  bool{
+fn expression(input: &mut VecDeque<lexer::Entry>) -> bool {
     //If it's eval
-   if peek(input, lexer::Token::Open) {
+    if peek(input, lexer::Token::Open) {
         evals(input)
-   }
-   //If it's a pure list
-   else if peek(input, lexer::Token::Pure) {
+    }
+    //If it's a pure list
+    else if peek(input, lexer::Token::Pure) {
         list(input)
-   }
-   else {
+    } else {
         false
-   }
+    }
 }
-
 
 pub fn parse(input: &mut VecDeque<lexer::Entry>) -> bool {
     return expression_list(input);
 }
-
-

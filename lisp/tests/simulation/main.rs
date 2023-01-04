baba7327d0e1601d93a0a84651d197cdc6f3ddf8
Anton Johansson <antojohansson1@gmail.com>
2023-01-04 17:34:12 +0100

    Made a basic way of trying snippets of code Also added a really crude random number generator. It should not really be used under any circumstance other than testing

---
+++ lisp/tests/simulation/main.rs
@@ -0,0 +1,44 @@
+pub mod utils;
+mod operators;
+
+
+//Test a simple addition
+#[test]
+fn sum_test() {
+
+    let a = utils::run_input("( + 1 1 ) ".to_string());
+    assert_eq!(a.literal,"2");
+
+}
+
+//Test the most simple of functions
+#[test]
+fn func_test() {
+
+    let input = vec!("( def a ( b c ) ( + b c ) )".to_string(), "( a 1 1 )".to_string());
+
+    let a = utils::run_multiple(input);
+    assert_eq!(a.literal,"2");
+
+}
+
+
+
+//Test a simple if case
+#[test]
+fn if_test() {
+
+    let input = vec!("( def a ( b c ) ( + b c ) )".to_string(),
+        "( if ( == ( a 2 3 ) 5 ) 2 0 ) ".to_string()
+        );
+
+    let a = utils::run_multiple(input);
+    assert_eq!(a.literal,"2");
+
+}
+
+
+
+
+
+
---
+++ lisp/tests/simulation/operators.rs
@@ -0,0 +1,63 @@
+use super::utils;
+use std::time::SystemTime;
+
+
+
+fn ints_to_input(input: &Vec<u32> ) -> String {
+
+    let mut to_return : String = "".to_string();
+
+    for i in input {
+        to_return.push_str(i.to_string().as_str());
+        to_return.push_str(" ");
+    }
+    to_return
+}
+
+
+#[test]
+fn addition() {
+
+    let i = "( + 1 2 3 4 5 6 )".to_string();
+    let res = utils::run_input(i);
+
+    assert_eq!(res.literal, (1 + 2 + 3 + 4 + 5 + 6 ).to_string());
+
+}
+
+fn random_addition() {
+
+    let mut input: Vec<u32> = Vec::new();
+    let mut rand: utils::Fakerand = utils::Fakerand {value: 1};
+    rand.seed(SystemTime::UNIX_EPOCH.elapsed().unwrap().as_millis());
+
+
+    for _ in 0..10 {
+        input.push(rand.get_uint() as u32);
+    }
+    let mut sum: i128 = 0;
+
+    for i in &input {
+        sum += *i as i128;
+    }
+
+    let mut to_operate: String = String::new();
+    
+    to_operate.push_str("( + ");
+    to_operate.push_str(ints_to_input(&input).as_str());
+    to_operate.push_str(" )");
+
+    let res = utils::run_input(to_operate);
+    
+    assert_eq!(sum.to_string(), res.literal);
+
+}
+
+//Run a random addition 100 times just to see if something ends up wrong
+#[test]
+fn more_addition() {
+    for _ in 0..100 {
+        random_addition();
+    }
+}
+
